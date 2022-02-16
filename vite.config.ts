52e804a12f4e664105cf50a2897a56717a8464b4
Alex Kim <45559664+alex-kim-dev@users.noreply.github.com>
2022-02-16 21:55:53 +0500

    build: add some vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createHtmlPlugin as html } from 'vite-plugin-html'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import svgr from 'vite-plugin-svgr'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(() => ({
+  plugins: [
+    react(),
+    html({
+      entry: 'src/main.tsx',
+      template: 'src/index.html',
+      minify: true,
+    }),
+    tsconfigPaths(),
+    svgr(),
+  ],
+}));
