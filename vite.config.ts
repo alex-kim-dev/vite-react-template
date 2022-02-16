e12350191bff82e89b6a26cc3f9b51c010b2337a
Alex Kim <45559664+alex-kim-dev@users.noreply.github.com>
2022-02-16 23:55:08 +0500

    style: format files with prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { createHtmlPlugin as html } from 'vite-plugin-html'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import svgr from 'vite-plugin-svgr'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createHtmlPlugin as html } from 'vite-plugin-html';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig(() => ({
   plugins: [
