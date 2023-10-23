ae8faaf704731fcf21cec47c6c0469201590f907
Alex Kim <45559664+alex-kim-dev@users.noreply.github.com>
2023-10-23 21:31:36 +0500

    chore: replace stale packages & update configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import { createHtmlPlugin as html } from 'vite-plugin-html';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(() => ({
-  plugins: [
-    react(),
-    html({
-      entry: '/src/main.tsx',
-      template: 'src/index.html',
-      minify: true,
-    }),
-    tsconfigPaths(),
-    svgr(),
-  ],
+  plugins: [react(), tsconfigPaths(), svgr()],
 
   test: {
     globals: true,
