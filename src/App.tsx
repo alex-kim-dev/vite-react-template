9fbfd6a9d8343ff72f63d2cee15f3a32fe73e906
Alex Kim <45559664+alex-kim-dev@users.noreply.github.com>
2025-07-05 15:13:56 +0500

    chore: update template

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,17 @@ import viteLogo from '/vite.svg'
 import css from '~/App.module.css'
 import ReactLogo from '~/assets/react.svg?react'
 
-export const App: React.FC = () => {
+export const App = () => {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href='https://vitejs.dev' target='_blank'>
+        <a href='https://vite.dev' target='_blank'>
           <img src={viteLogo} className={css.logo} alt='Vite logo' />
         </a>
         <a href='https://react.dev' target='_blank'>
-          <ReactLogo className={cn(css.logo, css.react)} />
+          <ReactLogo role='img' title='React logo' className={cn(css.logo, css.react)} />
         </a>
       </div>
       <h1>Vite + React</h1>
